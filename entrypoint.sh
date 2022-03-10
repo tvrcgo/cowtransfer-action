b43377e246eba0bab067aad97ec81b109855a3e1
tvrcgo <tvrcgo@gmail.com>
2022-03-10 11:10:39 +0800

    notice result

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ TAG=${GITHUB_REF##*/}-${GITHUB_RUN_ID}-$(date +"%s")
 
 curl -fsSL git.io/file-transfer | sh
 ./transfer cow --block 2621440 -s -p 64 --no-progress ${FILES} 2>&1 | tee cowtransfer.log
-echo "::warning file=CowTransfer::$(cat cowtransfer.log | grep https)"
+echo "::notice title=CowTransfer::$(cat cowtransfer.log | grep https)"
 echo "::set-output name=url::$(cat cowtransfer.log | grep https | cut -f3 -d" ")"
